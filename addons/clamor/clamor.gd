1f03597e0744ecbd4b7c7d71109547346de22839
SirLich <lrkoehler77@gmail.com>
2026-04-14 23:16:27 +0200

    Add context menu plugin for creating sounds

--- addons/clamor/clamor.gd
+++ addons/clamor/clamor.gd
@@ -5,19 +5,23 @@ class_name Clamor
 static var _settings_path = "addons/clamor/settings-path"
 const sound_library_editor = preload("uid://bvldqlmbuy0pa")
 const SoundToolbarInspectorPlugin = preload("uid://ch6cgsswgflbf")
+const CreateSoundsContextMenuPlugin = preload("uid://dxhh1jawtmivd")
 
 var sound_reference_editor_inspector_plugin : EditorInspectorPlugin
 var sound_library_editor_inspector_plugin : EditorInspectorPlugin
 var sound_toolbar_inspector_plugin : EditorInspectorPlugin
+var create_sounds_context_menu_plugin : EditorContextMenuPlugin
 
 func _enter_tree() -> void:
 	sound_reference_editor_inspector_plugin = load("uid://3srcdqbyo7qi").new()
 	sound_library_editor_inspector_plugin = sound_library_editor.new()
 	sound_toolbar_inspector_plugin = SoundToolbarInspectorPlugin.new()
+	create_sounds_context_menu_plugin = CreateSoundsContextMenuPlugin.new()
 	
 	add_inspector_plugin(sound_reference_editor_inspector_plugin)
 	add_inspector_plugin(sound_library_editor_inspector_plugin)
 	add_inspector_plugin(sound_toolbar_inspector_plugin)
+	add_context_menu_plugin(EditorContextMenuPlugin.CONTEXT_SLOT_FILESYSTEM, create_sounds_context_menu_plugin)
 	
 	add_autoload_singleton("SoundManager", "sound_manager.gd")
 	Clamor.initialize_settings()
@@ -34,6 +38,10 @@ func _exit_tree() -> void:
 	if is_instance_valid(sound_toolbar_inspector_plugin):
 		remove_inspector_plugin(sound_toolbar_inspector_plugin)
 		sound_toolbar_inspector_plugin = null
+		
+	if is_instance_valid(create_sounds_context_menu_plugin):
+		remove_context_menu_plugin(create_sounds_context_menu_plugin)
+		create_sounds_context_menu_plugin = null
 		
 	remove_autoload_singleton("SoundManager")
 
---
+++ addons/clamor/editor/editor_context_menu_plugin/create_sounds_context_menu_plugin.gd
@@ -0,0 +1,65 @@
+@tool
+
+extends EditorContextMenuPlugin
+
+func create_sound(args : Array[String]):
+	var sound = Sound.new()
+	var stream = get_streams(args)[0] as AudioStream
+	sound.stream = stream
+	
+	var base_path = args[0]
+	var root = base_path.get_base_dir()
+	var resource_name = base_path.get_file().get_basename().to_pascal_case()
+	var resource_path = "%s/%sSound.tres" % [root, resource_name]
+	
+	if FileAccess.file_exists(resource_path):
+		EditorInterface.get_editor_toaster().push_toast("Cannot create Sound: File already exists.", EditorToaster.SEVERITY_WARNING)
+		return
+		
+	ResourceSaver.save(sound, resource_path)
+
+func create_randomized_sound(args : Array[String]):
+	var randomizer = RandomizedSound.new()
+	var streams = get_streams(args)
+	randomizer.streams.append_array(streams)
+	
+	var base_path = args[0]
+	var root = base_path.get_base_dir()
+	var resource_name = base_path.get_file().get_basename().to_pascal_case()
+	var resource_path = "%s/%sRandomizedSound.tres" % [root, resource_name]
+	
+	if FileAccess.file_exists(resource_path):
+		EditorInterface.get_editor_toaster().push_toast("Cannot create randomizer: File already exists.", EditorToaster.SEVERITY_WARNING)
+		return
+		
+	ResourceSaver.save(randomizer, resource_path)
+	
+func get_streams(paths: Array[String]) -> Array[AudioStream]:
+	var streams : Array[AudioStream]
+	for path in paths:
+		if not FileAccess.file_exists(path):
+			continue
+			
+		var result = ResourceLoader.load(path)
+		if result is AudioStream:
+			streams.append(result)
+	return streams
+	
+func has_any_streams(paths: PackedStringArray) -> bool:
+	for path in paths:
+		if not FileAccess.file_exists(path):
+			continue
+			
+		var result = ResourceLoader.load(path)
+		if result is AudioStream:
+			return true
+	return false
+		
+func _popup_menu(paths: PackedStringArray) -> void:
+	if not has_any_streams(paths):
+		return
+		
+	if paths.size() == 1:
+		add_context_menu_item(&"Create Sound...", create_sound)
+	else:
+		add_context_menu_item(&"Create Randomized Sound...", create_randomized_sound)
