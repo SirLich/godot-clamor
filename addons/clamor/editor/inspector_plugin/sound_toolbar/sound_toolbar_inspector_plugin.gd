14866947d4d45314c216ef895fd0db8dc17351bc
SirLich <lrkoehler77@gmail.com>
2026-04-16 22:30:38 +0200

    Push

--- addons/clamor/editor/inspector_plugin/sound_toolbar/sound_toolbar_inspector_plugin.gd
+++ addons/clamor/editor/inspector_plugin/sound_toolbar/sound_toolbar_inspector_plugin.gd
@@ -3,10 +3,18 @@
 extends EditorInspectorPlugin
 
 const SoundToolbarEditorProperty = preload("uid://djnyk1wx284ur")
+const SOUND_TOOLBAR = preload("uid://bfg02p5mw772")
 
 func _can_handle(object: Object) -> bool:
 	return true
 
+func _parse_begin(object: Object) -> void:
+	if object is Sound:
+		var control = SOUND_TOOLBAR.instantiate()
+		control.configure(object)
+
+		add_custom_control(control)
+		
 func _parse_property(object: Object, type: Variant.Type, name: String, hint_type: PropertyHint, hint_string: String, usage_flags: int, wide: bool) -> bool:		
 	if name == "clamor_toolbar_section":
 		var control = SoundToolbarEditorProperty.new()
